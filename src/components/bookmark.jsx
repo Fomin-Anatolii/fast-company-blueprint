380580f5172c3afd386409bb11e1a27f1bdc45e0
Anatoly Fomin <58336508+Fomin-Anatolii@users.noreply.github.com>
2022-08-26 11:20:41 +0800

    add ESlint + PropTypes

--- src/components/bookmark.jsx
+++
@@ -1,9 +0,0 @@
-const Bookmark = ({ status, ...rest }) => {
-  return (
-    <button {...rest}>
-      <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "")}></i>
-    </button>
-  )
-}
-
-export default Bookmark
